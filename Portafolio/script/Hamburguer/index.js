9baa8b86195a941eda7838d7e5b5c543dbe28aed
LuisSilvaB <luissilvaballadares@hotmail.com>
2023-06-11 12:22:31 -0500

    Portafolio Refined (About) Me section structure

--- Portafolio/script/Hamburguer/index.js
+++ Portafolio/script/Hamburguer/index.js
@@ -4,7 +4,8 @@ export default function Hamburguer(idButton,idList) {
   const $button = d.getElementById(idButton);
   const $list = d.getElementById(idList);
   d.addEventListener('click', (e) => {
-    if (e.target == $button && $button.classList[0] == "Navbar__hamburguer_close") {
+    console.log($button.children);
+    if ((e.target == $button && $button.classList[0] == "Navbar__hamburguer_close") || e.target == $button.children[0] || e.target == $button.children[1] || e.target == $button.children[2]) {
       // * Animation button hamburguer 
       $button.classList.replace("Navbar__hamburguer_close","Navbar__hamburguer_open"); 
       if($list.classList[0] == "Navbar__list" ) $list.classList.replace("Navbar__list","Navbar__list-open"); 
