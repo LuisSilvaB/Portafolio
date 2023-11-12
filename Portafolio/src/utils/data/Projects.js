8cbf9bbdf9bf15afc8d89738160bd86423535e23
LuisSilvaB <LuisSilvaBalladares@hotmail.com>
2023-11-12 08:33:19 -0500

    feat: Add structure and styles (Projects section)

---
+++ Portafolio/src/utils/data/Projects.js
@@ -0,0 +1,11 @@
+const PROJECTS = [
+    {
+        id:'0001', 
+        title:'portafolio', 
+        description:'', 
+        technologies: ['html, css, javascript'], 
+        photoUrl:'',
+        repositoryUrl:'',
+        webUrl:'', 
+    }, 
+]
