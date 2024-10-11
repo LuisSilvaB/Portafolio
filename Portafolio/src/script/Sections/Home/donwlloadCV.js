39f5d73bde5e72584c813f62de0c93056979e7d4
LuisSilvaB <luisarmandoballadares@gmail.com>
2024-10-11 17:23:46 -0500

    chore: add download cv

--- Portafolio/src/script/Sections/Home/donwlloadCV.js
+++ Portafolio/src/script/Sections/Home/donwlloadCV.js
@@ -1,15 +1,16 @@
-
 export const downloadCv = () => {
   const buttonCv = document.getElementById('Button-Cv');
   
   if (buttonCv) {
-    buttonCv.addEventListener('click', function() {
-      window.open(buttonCv.href, '_blank');
-    });
-  
     buttonCv.addEventListener('click', function(e) {
-      e.preventDefault(); 
-      window.open(buttonCv.href, '_blank');
+      e.preventDefault();
+
+      const link = document.createElement('a');
+      link.href = buttonCv.href; 
+      link.setAttribute('download', 'CV_LuisSilvaBalladares');
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
     });
   }
 }
